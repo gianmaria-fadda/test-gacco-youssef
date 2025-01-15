4bd04d284a57a68f96ddadff34b49f1a82533b47
theguffs <youssefabdelmalak01@gmail.com>
2025-01-15 17:45:51 +0100

    risolto esercizio 1 php

--- php/index.php
+++ php/index.php
@@ -3,13 +3,13 @@ class Book {
     public $title, $author, $price, $year;
 
     public function __construct($title, $author, $price, $year) {
-        $this->$title = $title;
-        $this->$author = $author;
-        $this->$price = $price;
-        $this->$year = $year;
+        $this->title = $title;
+        $this->author = $author;
+        $this->price = $price;
+        $this->year = $year;
     }
 }
 
 $book1 = new Book("Il Signore degli Anelli", "Tolkien", 29.99, 1954);
-echo "Libro: $book1->title, Autore: $book1->author";
+echo "Libro: $book1->title, Autore: $book1->author, Prezzo: $book1->price, Anno: $book1->year";
 
